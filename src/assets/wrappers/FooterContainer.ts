71b1e6b474689e9fc4f97e4a58403c92540299c9
Vinicius Bueno <buenov535@gmail.com>
2024-04-12 12:27:02 -0300

    feat: criado primeiro estilo para o container de footer

---
+++ src/assets/wrappers/FooterContainer.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export default FooterContainer;
