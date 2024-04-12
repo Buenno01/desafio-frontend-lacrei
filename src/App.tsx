37a28af867dca5fffc9d7283f9f3e1ce036a20ea
Vinicius Bueno <buenov535@gmail.com>
2024-04-12 14:55:13 -0300

    feat: adicionado botão que volta ao topo da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import { useRef } from 'react';
+import { IoIosArrowUp } from "react-icons/io";
 import Content from './components/Content';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import ScrollToTopBtn from './components/ScrollToTopBtn';
 
 function App() {
+  const topRef = useRef<HTMLDivElement>(null);
+
+  const handleScrollToTop = () => {
+    topRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }
   return (
-    <>
+    <div ref={ topRef } id="top">
       <Header />
       <Content />
       <Footer />
-    </>
+      <ScrollToTopBtn onClick={ handleScrollToTop }>
+        <IoIosArrowUp />
+      </ScrollToTopBtn>
+    </div>
   );
 }
 
