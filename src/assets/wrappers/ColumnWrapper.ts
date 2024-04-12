3b21db9feba2836375f70a7483042fa4d391fff3
Vinicius Bueno <buenov535@gmail.com>
2024-04-12 14:34:28 -0300

    feat: adicionadas informações adicionais ao footer

--- src/assets/wrappers/ColumnWrapper.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const ColumnWrapper = styled.div<{ gap?: string }>`
-  display: flex;
-  flex-direction: column;
-  gap: ${(props) => (props.gap ? props.gap : '1rem')};
-`;
-
-export default ColumnWrapper;
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,4 @@
+import FooterInformationsWrapper from '../../assets/wrappers/FooterInformationsWrapper';
 import Container from '../../assets/wrappers/Container';
 import FooterContainer from '../../assets/wrappers/FooterContainer';
 import FooterLeftSide from './FooterLeftSide';
@@ -10,6 +11,18 @@ function Footer() {
         <FooterLeftSide />
         <FooterRightSide />
       </FooterContainer>
+      <FooterInformationsWrapper>
+        <p>
+          A Lacrei não oferece tratamento médico emergencial. Em caso de emergência procure o hospital mais próximo.
+        </p>
+        <p>
+          Em caso de problemas psicológicos, ligue para 188 (CVV) ou acesse o site www.cvv.org.br
+        </p>
+        <br />
+        <p>
+          Copyright © 2021 Lacrei. Todos os direitos reservados. CNPJ: 51.265.351/0001-65
+        </p>
+      </FooterInformationsWrapper>
     </Container>
   )
 }
